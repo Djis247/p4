e0cc4259c966dde91f2e84528ffaf120c02286b2
Inna Klimova <iklimov1@email.cpcc.edu>
2023-09-29 12:35:18 -0400

    Added Css to p1

--- components/states/States.jsx
+++ components/states/States.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './States.css';
+import React from "react";
+import "./States.css";
 
 /**
  * Define States, a React component of Project 4, Problem 2. The model
@@ -9,13 +9,32 @@ import './States.css';
 class States extends React.Component {
   constructor(props) {
     super(props);
-    console.log('window.models.states', window.models.states);
+    console.log("window.models.states", window.models.states);
   }
 
   render() {
+    let filteredStates = window.models.states();
+    let searchText = "al";
+
+    if (searchText) {
+      filteredStates = filteredStates.filter(function (fs) {
+        return fs.toLowerCase().includes(searchText.toLowerCase());
+      });
+    }
+
+    const statesList = filteredStates.map(function (fs) {
+      return <li key={fs}>{fs}</li>;
+    });
+
     return (
       <div>
-        Replace this with the code for Project 4, Problem 2
+        {/* //Replace this with the code for Project 4, Problem 2 */}
+
+        {/* Add input field 
+            [ ] value of `searchText`
+            [ ] the field should update `searchText` in the onChange event
+        */}
+        <ul>{statesList}</ul>
       </div>
     );
   }
